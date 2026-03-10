80e7a2ab755167d4eed4839c29d292b954ff601f
Immanuel Joy <immanueljoy107@gmail.com>
2026-03-10 08:16:39 -0400

    first commit

---
+++ main.ts
@@ -0,0 +1,13 @@
+const userName: string = "Immanuel"// String
+const age : number = 23// Number
+const isStudent: boolean = false// Boolean
+const tasks: string[] = ["Task 1", "Task 2", "Task 3"]// Array
+const user: { name: string; id: number } = { name: "Chief", id: 1 }; // Object
+
+// console.log(`Name: ${userName} and I'm ${age} years old. Am I a student? ${isStudent}`)
+console.log("My tasks for today:")
+tasks.forEach((task, index) => {
+    console.log(`${index + 1}. ${task}`)
+    
+})
+console.log (`You have ${tasks.length} tasks`) 
